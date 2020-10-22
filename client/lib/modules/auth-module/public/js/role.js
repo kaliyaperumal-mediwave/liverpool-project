d37208a880238e36d4de146302feb46cc0a1ebc1
Thiru Prasath <thiru@mindwaveventures.com>
2020-10-22 15:02:09 +0530

    WIP section 1

--- client/lib/modules/auth-module/public/js/role.js
+++ client/lib/modules/auth-module/public/js/role.js
@@ -1,3 +1,6 @@
+
+const apiUrl = "http://0.0.0.0:3001/user/eligibility"
+
 $(document).ready(function () {
 
 
@@ -5,26 +8,57 @@ $(document).ready(function () {
     //     source: availableTags
     // });
 
-    $('#datepicker').datepicker();
+    var _self = this;
     var app = new Vue({
         el: '#role-form',
         mounted: function () {
-            console.log('this', this)
-            this.save()
+          ///  this.getGP();
         },
         data: {
+
+            gpListShow: [],
             elgibilityObj: {},
             submitForm: ""
         },
         methods: {
+
+            getGP() {
+                console.log("Er");
+                gpList = [];
+                $.ajax({
+                    url: "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?PrimaryRoleId=RO177",
+                    type: 'get',
+                    success: function (response) {
+
+                        this.gpListShow = response.Organisations;
+
+                        for (i = 0; i < this.gpListShow.length; i++) {
+
+                            gpList.push(this.gpListShow[i].Name)
+
+                        }
+
+                        $("#gpLocation").autocomplete({
+                            source: gpList
+                        });
+                        return;
+                    },
+                    error: function (err) {
+                        console.log(err)
+                    },
+                })
+
+            },
+
+
             onChange(event) {
                 var optionText = event.target.name;
 
-                console.log(optionText);
                 if (optionText == "role" && this.elgibilityObj.interpreter != undefined) {
                     this.elgibilityObj.interpreter = "";
                     this.elgibilityObj.childDob = "";
                     this.submitForm = "false";
+                    
                 }
 
                 if (optionText == "interpreter" && this.elgibilityObj.camhs != undefined) {
@@ -34,53 +68,52 @@ $(document).ready(function () {
                     this.submitForm = "false";
                 }
 
+                if (optionText == "camhsSelect") {
+                   
+                    this.getGP();
+                
+                }
                 if (optionText == "camhsSelect" && this.submitForm != undefined) {
+                 
                     this.submitForm = "false";
 
                 }
 
 
             },
-            changeDob(e) {
-                debugger;
-                console.log(e);
-                $('#datepicker').datepicker({
-                    clearBtn: true,
-                    format: "dd/mm/yyyy"
+            getAddress(e) {
+                $("#gpLocation").on("autocompleteclose", function (event, ui) {
+                    console.log('this', _self, app);
+                    if (e.target.value === '') {
+                        app.submitForm = "false";
+                    } else {
+                        app.elgibilityObj.registerd_gp = e.target.value
+                        app.submitForm = "true";
+                    }
                 });
+            },
+            changeDob(e) {
+                $('#datepicker').datepicker().on(picker_event, function (e) {
+                    console.log(e)
+                });;
             },
             changeGP() {
                 this.submitForm = "true";
             },
             save() {
-                var availableTags = [
-                    "ActionScript",
-                    "AppleScript",
-                    "Asp",
-                    "BASIC",
-                    "C",
-                    "C++",
-                    "Clojure",
-                    "COBOL",
-                    "ColdFusion",
-                    "Erlang",
-                    "Fortran",
-                    "Groovy",
-                    "Haskell",
-                    "Java",
-                    "JavaScript",
-                    "Lisp",
-                    "Perl",
-                    "PHP",
-                    "Python",
-                    "Ruby",
-                    "Scala",
-                    "Scheme"
-                ];
-                $("#gpLocation").autocomplete({
-                    source: availableTags
-                });
-                return;
+              console.log(this.elgibilityObj)
+
+              $.ajax({
+                url: apiUrl,
+                type: 'post',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: JSON.stringify(this.elgibilityObj),
+                success: function (data) {
+                   console.log(data)
+                },
+              
+            });
             }
 
         }
