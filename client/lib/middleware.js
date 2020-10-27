d9161190d9d662262f65141f0f0596b2a75cffc8
Thiru Prasath <thiru@mindwaveventures.com>
2020-10-27 15:24:02 +0530

    feat-added env

---
+++ client/lib/middleware.js
@@ -0,0 +1,163 @@
+const request = require('request-promise');
+
+module.exports = function (self, options) {
+  self.middleware = {
+    post: function (req, res, url, body) {
+      return new Promise((resolve, reject) => {
+        let options = {
+          method: 'POST',
+          uri: url,
+          body: body,
+          headers: self.setHeader(req),
+          json: true, // Automatically stringifies the body to JSON
+          rejectUnauthorized: false, // temp certificate validation false
+          resolveWithFullResponse: true // Get the full response instead of just the body
+        };
+        try {
+          request(options)
+            .then(function (response) {
+              resolve(response.body);
+            })
+            .catch(function (err) {
+              if (err.statusCode === 401 && req.session.auth_token) {
+                req.session.destroy(function () { });
+              } else if (err.error.code === 'ECONNREFUSED') {
+                err.statusCode = 500;
+              }
+              reject(err);
+            });
+        } catch (error) {
+          let err = {
+            statusCode: 408,
+            error: 'Request Timeout'
+          };
+          reject(err);
+        }
+
+      });
+    },
+
+    get: function (req, url) {
+      return new Promise((resolve, reject) => {
+        let options = {
+          method: 'GET',
+          uri: url,
+          headers: self.setHeader(req),
+          json: true, // Automatically stringifies the body to JSON
+          rejectUnauthorized: false, // temp certificate validation false
+          resolveWithFullResponse: true // Get the full response instead of just the body
+        };
+        try {
+          request(options)
+            .then(function (response) {
+              resolve(response.body);
+            })
+            .catch(function (err) {
+              if (err.statusCode === 401 && !req.session.aposBlessings) {
+                req.session.auth_token = null;
+                req.session.redirectto = req.url;
+                req.session.reload(function () { });
+                return req.res.redirect('/users/login');
+              } else {
+                err.statusCode = 500;
+              }
+              reject(err);
+            });
+        } catch (error) {
+          let err = {
+            statusCode: 408,
+            error: 'Request Timeout'
+          };
+          reject(err);
+        }
+      });
+    },
+
+    put: function (req, res, url, body) {
+      return new Promise((resolve, reject) => {
+        let options = {
+          method: 'PUT',
+          uri: url,
+          body: body,
+          headers: self.setHeader(req),
+          json: true, // Automatically stringifies the body to JSON
+          rejectUnauthorized: false, // temp certificate validation false
+          resolveWithFullResponse: true // Get the full response instead of just the body
+        };
+        try {
+          request(options)
+            .then(function (response) {
+              resolve(response.body);
+            })
+            .catch(function (err) {
+              if (err.statusCode === 401) {
+                req.session.destroy(function () { });
+              } else {
+                err.statusCode = 500;
+              }
+              reject(err);
+            });
+        } catch (error) {
+          let err = {
+            statusCode: 408,
+            error: 'Request Timeout'
+          };
+          reject(err);
+        }
+
+      });
+    },
+
+    delete: function (req, res, url, body) {
+      return new Promise((resolve, reject) => {
+        let options = {
+          method: 'DELETE',
+          uri: url,
+          body: body,
+          headers: self.setHeader(req),
+          json: true, // Automatically stringifies the body to JSON
+          rejectUnauthorized: false, // temp certificate validation false
+          resolveWithFullResponse: true // Get the full response instead of just the body
+        };
+        try {
+          request(options)
+            .then(function (response) {
+              resolve(response.body);
+            })
+            .catch(function (err) {
+              if (err.statusCode === 401) {
+                req.session.destroy(function () { });
+              } else {
+                err.statusCode = 500;
+              }
+              reject(err);
+            });
+        } catch (error) {
+          let err = {
+            statusCode: 408,
+            error: 'Request Timeout'
+          };
+          reject(err);
+        }
+
+      });
+    }
+  };
+
+
+  self.setHeader = function (req) {
+    let headers;
+    if (req.session.auth_token) {
+      headers = {
+        'Authorization': 'Bearer ' + req.session.auth_token,
+        'domain_url': req.baseUrl ? req.baseUrl : req.data.baseUrl
+      };
+    } else {
+      headers = {
+        'domain_url': req.baseUrl ? req.baseUrl : req.data.baseUrl
+      };
+    }
+    return headers;
+  };
+
+};
--- client/lib/modules/auth-module/index.js
+++ client/lib/modules/auth-module/index.js
@@ -11,5 +11,19 @@ module.exports = {
     self.role = function (req, callback) {
       return self.sendPage(req, self.renderer('role', {}));
     };
+    require('../../middleware')(self, options);
+    // save eligibitiy
+    self.route('post', 'eligibility', function (req, res) {
+      var url = self.apos.LIVERPOOLMODULE.getOption(req, 'phr-module')+ '/user/eligibility';
+      console.log("-------");
+      console.log(url);
+      console.log("-------");
+      self.middleware.post(req, res, url, req.body).then((data) => {
+         return res.send(data);
+      }).catch((error) => {
+         console.log("---- error -------", error)
+         return res.status(error.statusCode).send(error.error);
+      });
+   });
   }
 }
