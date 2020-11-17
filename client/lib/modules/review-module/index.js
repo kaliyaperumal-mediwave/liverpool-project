26f57dc6140404cc2dd29971e5d00b80f599a4f2
Thiru Prasath <tprasath40@gmail.com>
2020-11-17 19:19:39 +0530

    feat-sec5-integration1

--- client/lib/modules/review-module/index.js
+++ client/lib/modules/review-module/index.js
@@ -14,5 +14,32 @@ module.exports = {
         subContent: ""
       }));
     };
+
+    require('../../middleware')(self, options);
+    // save eligibitiy
+    self.route('post', 'fetchReview', function (req, res) {
+      var url = self.apos.LIVERPOOLMODULE.getOption(req, 'phr-module')+ '/user/fetchReview';
+      console.log("-------");
+      console.log(url);
+      console.log("-------");
+      self.middleware.post(req, res, url, req.body).then((data) => {
+         return res.send(data);
+      }).catch((error) => {
+         console.log("---- error -------", error)
+         return res.status(error.statusCode).send(error.error);
+      });
+   });
+   self.route('post', 'saveReview', function (req, res) {
+    var url = self.apos.LIVERPOOLMODULE.getOption(req, 'phr-module')+ '/user/saveReview';
+    console.log("-------");
+    console.log(url);
+    console.log("-------");
+    self.middleware.post(req, res, url, req.body).then((data) => {
+       return res.send(data);
+    }).catch((error) => {
+       console.log("---- error -------", error)
+       return res.status(error.statusCode).send(error.error);
+    });
+ });
   }
 }
