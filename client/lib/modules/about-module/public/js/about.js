26727244aae5a0ff6fa5f98dfa70d6c3053f8d4b
RajkumarManogar <rajkumar@mindwavevemtures.com>
2020-12-09 20:17:01 +0530

    feat-added optional text as bold

--- client/lib/modules/about-module/public/js/about.js
+++ client/lib/modules/about-module/public/js/about.js
@@ -84,12 +84,12 @@ $(document).ready(function () {
                 },
             });
 
-            if (this.paramValues[2] !=undefined) {
+            if (this.paramValues[2] != undefined) {
                 this.fetchSavedData();
             }
 
             this.initMaps()
-           
+
         },
         methods: {
 
@@ -244,8 +244,9 @@ $(document).ready(function () {
                 this.isFormSubmitted = true;
                 var formData = Object.assign(this.aboutObj, this.aboutFormData);
                 if (formData.contactNumber && formData.relationshipToYou &&
-                    formData.parentialResponsibility && formData.childGender &&
-                    formData.childIdentity && formData.sendPost && formData.childAddress && formData.childName && this.phoneRegex.test(formData.contactNumber)
+                    formData.parentialResponsibility && formData.childGender && formData.parentName &&
+                    formData.childIdentity && formData.sendPost && formData.childAddress && formData.childName && formData.childContactNumber
+                    && this.phoneRegex.test(formData.contactNumber) && this.phoneRegex.test(formData.childContactNumber)
                 ) {
 
                     if (formData.parentialResponsibility == 'no' && !formData.parentCarerName) {
@@ -253,28 +254,42 @@ $(document).ready(function () {
                         return false;
                     }
 
-                    // if ((formData.nhsNumber && !this.nhsRegex.test(formData.nhsNumber)) || (formData.childEmail && !this.emailRegex.test(formData.childEmail)) ||
-                    //     (formData.childContactNumber && !this.phoneRegex.test(formData.childContactNumber)) ||
-                    //     (formData.contactNumber && !this.phoneRegex.test(formData.contactNumber)) ||
-                    //     (formData.emailAddress && !this.emailRegex.test(formData.emailAddress))
-                    // )  {
-                    //     scrollToInvalidInput();
-                    //     return false;
-                    // } 
-                    else {
-                        this.payloadData.aboutData = JSON.parse(JSON.stringify(formData));
-                        this.payloadData.role = this.userRole;
-                        this.payloadData.userid = this.userId;
-                        this.payloadData.allHouseHoldMembers = this.allHouseHoldMembers;
-                        this.payloadData.editFlag = this.paramValues[2]
-                        //   this.payloadData.id = this.referralId;
-                        if (this.userMode === 'edit') {
-                            this.payloadData.userMode = 'edit';
-                        } else {
-                            this.payloadData.userMode = 'add';
-                        }
-                        this.upsertAboutYouForm(this.payloadData);
+                    if ((formData.nhsNumber && !this.nhsRegex.test(formData.nhsNumber))) {
+                        scrollToInvalidInput();
+                        return false;
                     }
+
+                    if ((formData.childEmail && !this.emailRegex.test(formData.childEmail))) {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+
+                    if ((formData.childContactNumber && !this.phoneRegex.test(formData.childContactNumber))) {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+
+                    if ((formData.contactNumber && !this.phoneRegex.test(formData.contactNumber))) {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+
+                    if ((formData.emailAddress && !this.emailRegex.test(formData.emailAddress))) {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+
+                    this.payloadData.aboutData = JSON.parse(JSON.stringify(formData));
+                    this.payloadData.role = this.userRole;
+                    this.payloadData.userid = this.userId;
+                    this.payloadData.allHouseHoldMembers = this.allHouseHoldMembers;
+                    this.payloadData.editFlag = this.paramValues[2]
+                    if (this.userMode === 'edit') {
+                        this.payloadData.userMode = 'edit';
+                    } else {
+                        this.payloadData.userMode = 'add';
+                    }
+                    this.upsertAboutYouForm(this.payloadData);
 
                 } else {
                     scrollToInvalidInput();
@@ -287,9 +302,9 @@ $(document).ready(function () {
             upsertAboutYouForm: function (payload) {
                 //   console.log(payload);
                 var responseData = apiCallPost('post', '/saveReferral', payload);
-                   // console.log(responseData);
+                // console.log(responseData);
                 if (Object.keys(responseData)) {
-                    location.href =redirectUrl(location.href,"education",this.userId,this.userRole);
+                    location.href = redirectUrl(location.href, "education", this.userId, this.userRole);
                     // if (this.paramValues[2] == undefined) {
                     //     var parameter = this.userId + "&" + this.userRole
                     //     var enCodeParameter = btoa(parameter)
@@ -404,7 +419,6 @@ $(document).ready(function () {
             clearDate: function (e) {
                 if (e.keyCode == 8 || e.keyCode == 46) {
                     $('#houseHoldDate').datepicker('setDate', null);
-                    // $('input.hasDatepicker').val('');
                     this.houseHoldData.dob = "";
                 }
             },
@@ -430,8 +444,6 @@ $(document).ready(function () {
                 this.fetchAgeLogic(dbDate);
                 return yyyy + '-' + (mmChars[1] ? mm : "0" + mmChars[0]) + '-' + (ddChars[1] ? dd : "0" + ddChars[0]);
             },
-
-
         }
 
     })
