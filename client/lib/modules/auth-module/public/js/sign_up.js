51fe826c1d05cf5ce2e4ba4467677e79b5e6a21e
selvanmindwave <selvan@mindwaveevntures.com>
2020-12-23 15:08:54 +0530

    updated home page logo

--- client/lib/modules/auth-module/public/js/sign_up.js
+++ client/lib/modules/auth-module/public/js/sign_up.js
@@ -1,10 +1,7 @@
-//const apiUrl = "/user/eligibility"
 var API_URI = "/modules/auth-module";
 $(document).ready(function () {
 
-    Vue.component('date-picker', VueBootstrapDatetimePicker);
-    var _self = this;
-    var app = new Vue({
+    new Vue({
         el: '#user_sign_up',
 
         data: {
@@ -12,12 +9,13 @@ $(document).ready(function () {
                 first_name: "",
                 last_name: "",
                 password: "",
-                confirm_password:"",
-                email:""
+                confirm_password: "",
+                email: ""
             },
             isFormSubmitted: false,
             emailRegex: /^[a-z-0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,7}$/i,
-            samePass:true
+            passwordRegex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&?*-]).{8,}$/,
+            samePass: true
         },
 
         mounted: function () {
@@ -28,15 +26,15 @@ $(document).ready(function () {
             submitSignIn: function () {
                 let formData = this.signUpObject;
                 this.isFormSubmitted = true
-                
-                if ((formData.first_name && formData.last_name && formData.password&&formData.confirm_password&&formData.email&&this.emailRegex.test(formData.email)&&(formData.password===formData.confirm_password))) {
+
+                if ((formData.first_name && formData.last_name && formData.password&&this.passwordRegex.test(formData.password) && formData.confirm_password&&this.passwordRegex.test(formData.confirm_password) && formData.email && this.emailRegex.test(formData.email) && (formData.password === formData.confirm_password))) {
                     console.log('payload', formData);
-                        var successData = apiCallPost('post', '/doCreateAcc', formData);
-                        if (Object.keys(successData)) {
-                            console.log(successData);
-                        } else {
-                            console.log('empty response')
-                        }
+                    var successData = apiCallPost('post', '/doCreateAcc', formData);
+                    if (Object.keys(successData)) {
+                        console.log(successData);
+                    } else {
+                        console.log('empty response')
+                    }
                 } else {
                     scrollToInvalidInput();
                     return false;
