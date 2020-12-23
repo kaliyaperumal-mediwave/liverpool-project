692608bd9465a02695275a8ca5ab27c1464ef27e
selvanmindwave <selvan@mindwaveevntures.com>
2020-12-23 12:34:27 +0530

    updated css

---
+++ client/lib/modules/resources-module/public/js/resources.js
@@ -0,0 +1,45 @@
+//const apiUrl = "/user/eligibility"
+var API_URI = "/modules/auth-module";
+$(document).ready(function () {
+
+    Vue.component('date-picker', VueBootstrapDatetimePicker);
+    var _self = this;
+    var app = new Vue({
+        el: '#resource',
+
+        data: {
+            loginObject: {
+                email: "",
+                password: ""
+            },
+            isFormSubmitted: false,
+            emailRegex: /^[a-z-0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,7}$/i,
+        },
+
+        mounted: function () {
+
+        },
+
+        methods: {
+            submitLogin: function () {
+                let formData = this.loginObject;
+                this.isFormSubmitted = true
+                if ((formData.email && formData.password && this.emailRegex.test(formData.email))) {
+                    console.log('payload', formData);
+                    var successData = apiCallPost('post', '/fetchProfession', formData);
+                    if (Object.keys(successData)) {
+                        console.log(successData);
+                    } else {
+                        console.log('empty response')
+                    }
+
+                } else {
+                    scrollToInvalidInput();
+                    return false;
+                }
+            },
+
+        }
+    })
+
+});
