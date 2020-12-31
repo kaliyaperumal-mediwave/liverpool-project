99452f002d4ec6517f0740cd913b860034d97f06
selvanmindwave <selvan@mindwaveevntures.com>
2020-12-31 11:11:50 +0530

    updated check-referral

--- client/lib/modules/check-referral-module/public/js/check-referral.js
+++ client/lib/modules/check-referral-module/public/js/check-referral.js
@@ -5,52 +5,27 @@ $(document).ready(function () {
         el: '#check-referral',
 
         data: {
-            loginObject: {
+            checkReferral: {
                 email: "",
                 password: ""
             },
             isFormSubmitted: false,
             showVisibility: false,
-            emailRegex: /^[a-z-0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,7}$/i,
-            passwordRegex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&?*-]).{8,}$/,
         },
 
         mounted: function () {
         },
 
         methods: {
-
-            submitLogin: function () {
-                let formData = this.loginObject;
-                this.isFormSubmitted = true
-                if ((formData.email && formData.password && this.emailRegex.test(formData.email) && this.passwordRegex.test(formData.password))) {
-                    console.log('payload', formData);
-                    var successData = apiCallPost('post', '/doLogin', formData);
-                    if (Object.keys(successData)) {
-                        console.log(successData);
-                    } else {
-                        console.log('empty response')
-                    }
-
+            toggleArrow: function (e) {
+                var ele = e.target;
+                var classList = Array.from(e.target.classList)
+                if (classList.indexOf('fa-chevron-circle-up') > -1) {
+                    $(ele).removeClass('fa-chevron-circle-up').addClass('fa-chevron-circle-down');
                 } else {
-                    scrollToInvalidInput();
-                    return false;
+                    $(ele).removeClass('fa-chevron-circle-down').addClass('fa-chevron-circle-up');
                 }
             },
-
-            toggleVisibility: function () {
-                this.showVisibility = !this.showVisibility;
-                if ($('#loginPassword').attr("type") == "text") {
-                    $('#loginPassword').attr('type', 'password');
-                } else if ($('#loginPassword').attr("type") == "password") {
-                    $('#loginPassword').attr('type', 'text');
-                }
-            },
-
-            navigatePage: function (route) {
-                window.location.href = window.location.origin + route;
-            },
-
         }
     })
 
