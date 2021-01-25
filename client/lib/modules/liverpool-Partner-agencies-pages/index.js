506312dead46d0b99962ecdc073475f313077b62
kathir <kathir@mindwaveventures.com>
2021-01-25 15:58:31 +0530

    [ADD] display upload time on all pieces page

--- client/lib/modules/liverpool-Partner-agencies-pages/index.js
+++ client/lib/modules/liverpool-Partner-agencies-pages/index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+const moment = require('moment');
 
 module.exports = {
   name: 'liverpool-Partner-agencies-pages',
@@ -24,7 +25,29 @@ module.exports = {
       require('../../middleware')(self, options);
       if(req.query && req.query.piece_id) {
         const pieces = [];
+        let todayDate = moment().format('DD-MM-YYYY HH:MM:SS');
         for(let index = 0; index < req.data.pieces.length; index++) {
+          let videoTime = moment(req.data.pieces[index].createdAt).format('DD-MM-YYYY HH:MM:SS');
+          let value = moment(todayDate, 'DD-MM-YYYY HH:MM:SS').diff(moment(videoTime, 'DD-MM-YYYY HH:MM:SS'), 'days');
+          let time = '';
+          if(value < 1) {
+              value = moment(todayDate, 'DD-MM-YYYY HH:MM:SS').diff(moment(videoTime, 'DD-MM-YYYY HH:MM:SS'), 'hours');
+              if(value == 1) {
+                  value = moment(todayDate, 'DD-MM-YYYY HH:MM:SS').diff(moment(videoTime, 'DD-MM-YYYY HH:MM:SS'), 'minutes');
+                  time = 'minute';
+              } else {
+                  time = 'hour';
+              }
+          } else if(value < 30) {
+              time = 'day';
+          } else if(value < 365) {
+              value = moment(todayDate, 'DD-MM-YYYY HH:MM:SS').diff(moment(videoTime, 'DD-MM-YYYY HH:MM:SS'), 'months');
+              time = 'month';
+          } else {
+              value = moment(todayDate, 'DD-MM-YYYY HH:MM:SS').diff(moment(videoTime, 'DD-MM-YYYY HH:MM:SS'), 'years');
+              time = 'year';
+          }
+          req.data.pieces[index].uploadTime = setVideoValue(value, time);
           if(req.data.pieces[index]._id == req.query.piece_id) {
             pieces.splice(0, 0, req.data.pieces[index]);
           } else {
