d10d1671e426a522ac02bfdc310963e30c9b8a84
priyanka246 <priyanka@mindwaveventures.com>
2021-01-27 21:02:28 +0530

    [ADD]: filter

---
+++ client/lib/modules/apostrophe-assets/public/js/filters.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function () {
+    new Vue({
+        el: '#filter_content',
+        data: {
+            searchQuery: null,
+            filteredData: [],
+            showSearchResults: false,
+            resources: [],
+        },
+
+        mounted: function () {
+            this.resources = JSON.parse(document.getElementById('resources').value)
+        },
+
+        methods: {
+            filterPieces: function () {
+                console.log(this.searchQuery, "this.searchQuerythis.searchQuery");
+                if (this.searchQuery) {
+                    this.filteredData = [];
+                    this.showSearchResults = true;
+                    return this.resources.filter((item) => {
+                        // TODO: add description and other content after CMS
+                        if (this.searchQuery.toLowerCase().split(' ').every(v => item.title.toLowerCase().includes(v))) {
+                            this.filteredData.push(item)
+                        }
+                        return this.filteredData
+                    })
+                } else {
+                    this.showSearchResults = false;
+                    return this.filteredData = [];
+                }
+            },
+        },
+    })
+
+});
