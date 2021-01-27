96e871ce2071024f1d54e5473fd5e80b75eae7db
priyanka246 <priyanka@mindwaveventures.com>
2021-01-27 20:35:53 +0530

    [FIX]: Resource level 1 search

--- client/lib/modules/Resources-pages/index.js
+++ client/lib/modules/Resources-pages/index.js
@@ -20,17 +20,15 @@ module.exports = {
     var superBefore = self.beforeShow;
     self.beforeShow = function (req, callback) {
       require('../../middleware')(self, options);
-
+      console.log("show==========");
       self.checkCommonPageAuth(req).then(async (req) => {
         let piecesArray = [];
-        var Resources = await self.apos.modules['Resources-pages'].pieces.find(req, {}).toArray();
-        var ThingsToWatch = await self.apos.modules['liverpool-watch-pages'].pieces.find(req, {}).toArray();
-        var ThingsToRead = await self.apos.modules['liverpool-read-pages'].pieces.find(req, {}).toArray();
-        var Games = await self.apos.modules['liverpool-games-pages'].pieces.find(req, {}).toArray();
-        var Events = await self.apos.modules['liverpool-events-pages'].pieces.find(req, {}).toArray();
-        var PartnerAgencies = await self.apos.modules['liverpool-Partner-agencies-pages'].pieces.find(req, {}).toArray();
-
-        piecesArray = Resources.concat(ThingsToWatch, ThingsToRead, Games, Events, PartnerAgencies)
+        var ThingsToWatch = req.data.piece._watchPage.length > 0 ? req.data.piece._watchPage : [];
+        var ThingsToRead = req.data.piece._readPage.length > 0 ? req.data.piece._readPage : [];
+        var Games = req.data.piece._gamesPage.length > 0 ? req.data.piece._gamesPage : [];
+        var Events = req.data.piece._eventPage.length > 0 ? req.data.piece._eventPage : [];
+        var PartnerAgencies = req.data.piece._partnerAgenciesPage.length > 0 ? req.data.piece._partnerAgenciesPage : [];
+        piecesArray = ThingsToWatch.concat(ThingsToRead, Games, Events, PartnerAgencies)
         req.data.piecesArray = piecesArray;
         return superBefore(req, callback);
       }).catch(() => {
@@ -42,14 +40,6 @@ module.exports = {
       require('../../middleware')(self, options);
 
       self.checkCommonPageAuth(req).then(async (req) => {
-        // let piecesArray = [];
-        // var ThingsToWatch = req.data.piece._watchPage;
-        // var ThingsToRead = req.data.piece._readPage;
-        // var Games = req.data.piece._gamesPage;
-        // var Events = req.data.piece._eventPage;
-        // var PartnerAgencies = req.data.piece._partnerAgenciesPage;
-        // piecesArray = ThingsToWatch.concat(ThingsToRead, Games, Events, PartnerAgencies)
-        // req.data.piecesArray = piecesArray;
         let piecesArray = [];
         var Resources = await self.apos.modules['Resources-pages'].pieces.find(req, {}).toArray();
         var ThingsToWatch = await self.apos.modules['liverpool-watch-pages'].pieces.find(req, {}).toArray();
