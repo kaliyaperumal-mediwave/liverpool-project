70c26fdc5cbc7d2c67bdbc28e78c91e32dbb4e62
priyanka246 <priyanka@mindwaveventures.com>
2021-01-27 20:29:25 +0530

    [FIX]:Resources page search

--- client/lib/modules/Resources-pages/index.js
+++ client/lib/modules/Resources-pages/index.js
@@ -23,12 +23,14 @@ module.exports = {
 
       self.checkCommonPageAuth(req).then(async (req) => {
         let piecesArray = [];
-        var ThingsToWatch = req.data.piece._watchPage;
-        var ThingsToRead = req.data.piece._readPage;
-        var Games = req.data.piece._gamesPage;
-        var Events = req.data.piece._eventPage;
-        var PartnerAgencies = req.data.piece._partnerAgenciesPage;
-        piecesArray = ThingsToWatch.concat(ThingsToRead, Games, Events, PartnerAgencies)
+        var Resources = await self.apos.modules['Resources-pages'].pieces.find(req, {}).toArray();
+        var ThingsToWatch = await self.apos.modules['liverpool-watch-pages'].pieces.find(req, {}).toArray();
+        var ThingsToRead = await self.apos.modules['liverpool-read-pages'].pieces.find(req, {}).toArray();
+        var Games = await self.apos.modules['liverpool-games-pages'].pieces.find(req, {}).toArray();
+        var Events = await self.apos.modules['liverpool-events-pages'].pieces.find(req, {}).toArray();
+        var PartnerAgencies = await self.apos.modules['liverpool-Partner-agencies-pages'].pieces.find(req, {}).toArray();
+
+        piecesArray = Resources.concat(ThingsToWatch, ThingsToRead, Games, Events, PartnerAgencies)
         req.data.piecesArray = piecesArray;
         return superBefore(req, callback);
       }).catch(() => {
@@ -40,13 +42,23 @@ module.exports = {
       require('../../middleware')(self, options);
 
       self.checkCommonPageAuth(req).then(async (req) => {
+        // let piecesArray = [];
+        // var ThingsToWatch = req.data.piece._watchPage;
+        // var ThingsToRead = req.data.piece._readPage;
+        // var Games = req.data.piece._gamesPage;
+        // var Events = req.data.piece._eventPage;
+        // var PartnerAgencies = req.data.piece._partnerAgenciesPage;
+        // piecesArray = ThingsToWatch.concat(ThingsToRead, Games, Events, PartnerAgencies)
+        // req.data.piecesArray = piecesArray;
         let piecesArray = [];
-        var ThingsToWatch = req.data.piece._watchPage;
-        var ThingsToRead = req.data.piece._readPage;
-        var Games = req.data.piece._gamesPage;
-        var Events = req.data.piece._eventPage;
-        var PartnerAgencies = req.data.piece._partnerAgenciesPage;
-        piecesArray = ThingsToWatch.concat(ThingsToRead, Games, Events, PartnerAgencies)
+        var Resources = await self.apos.modules['Resources-pages'].pieces.find(req, {}).toArray();
+        var ThingsToWatch = await self.apos.modules['liverpool-watch-pages'].pieces.find(req, {}).toArray();
+        var ThingsToRead = await self.apos.modules['liverpool-read-pages'].pieces.find(req, {}).toArray();
+        var Games = await self.apos.modules['liverpool-games-pages'].pieces.find(req, {}).toArray();
+        var Events = await self.apos.modules['liverpool-events-pages'].pieces.find(req, {}).toArray();
+        var PartnerAgencies = await self.apos.modules['liverpool-Partner-agencies-pages'].pieces.find(req, {}).toArray();
+
+        piecesArray = Resources.concat(ThingsToWatch, ThingsToRead, Games, Events, PartnerAgencies)
         req.data.piecesArray = piecesArray;
         return beforeIndex(req, callback);
       }).catch(() => {
