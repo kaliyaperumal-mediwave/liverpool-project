5c14857cc871ceefdc592d173a5467aaa4b0db39
priyanka246 <priyanka@mindwaveventures.com>
2021-01-28 18:07:44 +0530

    [ADD]: people filter

--- client/lib/modules/liverpool-mental-health-pages/index.js
+++ client/lib/modules/liverpool-mental-health-pages/index.js
@@ -5,24 +5,35 @@ module.exports = {
   extend: 'apostrophe-pieces-pages',
   label: 'Mental-Health-Page',
   piecesFilters: [
-    { name: 'tags',
-  counts: true }
-],
-  construct: function(self, options) {
+    {
+      name: 'tags',
+      counts: true
+    }
+  ],
+  construct: function (self, options) {
 
     var superBefore = self.beforeShow;
-    self.beforeShow = function(req, callback) {
+    self.beforeShow = function (req, callback) {
       require('../../middleware')(self, options);
-      self.checkCommonPageAuth(req).then((req) => {
+      self.checkCommonPageAuth(req).then(async (req) => {
+        req.data.piecesArray = _.map(req.data.pieces, (item) => {
+          item.custom_url = "/mental-health/people?piece_id=" + item._id
+          return item;
+        })
         return superBefore(req, callback);
       }).catch(() => {
       });
     };
 
     var beforeIndex = self.beforeIndex;
-    self.beforeIndex = function(req, callback) {
+    self.beforeIndex = function (req, callback) {
       require('../../middleware')(self, options);
-          self.checkCommonPageAuth(req).then((req) => {
+
+      self.checkCommonPageAuth(req).then(async (req) => {
+        req.data.piecesArray = _.map(req.data.pieces, (item) => {
+          item.custom_url = "/mental-health/people?piece_id=" + item._id
+          return item;
+        })
         return beforeIndex(req, callback);
       }).catch(() => {
       });
