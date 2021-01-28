35d3e2384466a5f350bb825466d448614271d0c7
priyanka246 <priyanka@mindwaveventures.com>
2021-01-28 19:25:30 +0530

    [ADD]: filter in home

--- client/lib/modules/home-module/index.js
+++ client/lib/modules/home-module/index.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 module.exports = {
   extend: 'apostrophe-custom-pages',
   label: 'Home Module',
@@ -7,21 +8,50 @@ module.exports = {
   construct: function (self, options) {
     require('../../middleware')(self, options);
     self.addDispatchRoutes = function () {
-      self.dispatch('/',self.middleware.checkCommonPageAuth, self.home);
+      self.dispatch('/', self.middleware.checkCommonPageAuth, self.home);
     };
-    self.home = function (req, callback) {
+    self.home = async function (req, callback) {
+      var Resources = await self.apos.modules['Resources-pages'].pieces.find(req, {}).toArray();
+      var ThingsToWatchArray = await self.apos.modules['liverpool-watch-pages'].pieces.find(req, {}).toArray();
+      var ThingsToWatch = _.map(ThingsToWatchArray, (item) => {
+        item.custom_url = "/watch?piece_id=" + item._id
+        return item;
+      })
+      var ThingsToReadArray = await self.apos.modules['liverpool-read-pages'].pieces.find(req, {}).toArray();
+      var ThingsToRead = _.map(ThingsToReadArray, (item) => {
+        item.custom_url = "/read?piece_id=" + item._id
+        return item;
+      })
+      var GamesArray = await self.apos.modules['liverpool-games-pages'].pieces.find(req, {}).toArray();
+      var Games = _.map(GamesArray, (item) => {
+        item.custom_url = "/games?piece_id=" + item._id
+        return item;
+      })
+      var EventsArray = await self.apos.modules['liverpool-events-pages'].pieces.find(req, {}).toArray();
+      var Events = _.map(EventsArray, (item) => {
+        item.custom_url = "/events?piece_id=" + item._id
+        return item;
+      })
 
+      var PartnerAgenciesArray = await self.apos.modules['liverpool-Partner-agencies-pages'].pieces.find(req, {}).toArray();
+      var PartnerAgencies = _.map(PartnerAgenciesArray, (item) => {
+        item.custom_url = "/partner?piece_id=" + item._id
+        return item;
+      })
+      var AboutService = await self.apos.modules['liverpool-about-service-pages'].pieces.find(req, {}).toArray();
+      var PeopleService = await self.apos.modules['liverpool-mental-health-pages'].pieces.find(req, {}).toArray();
+      piecesArray = Resources.concat(ThingsToWatch, ThingsToRead, Games, Events, PartnerAgencies, AboutService, PeopleService)
+      req.data.piecesArray = piecesArray;
       // check already logged user 
       // if yes redirect user to dashboard directly else redirect them to home page
       req.res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate'); //This will force the browser to obtain new copy of the page even when they hit "back".
-      if(req.session.auth_token)
-      {
+      if (req.session.auth_token) {
         return req.res.redirect("/dashboard");
       }
-      else
-      {
+      else {
         return self.sendPage(req, self.renderer('home', {
           showHeader: false,
+          piecesArray: piecesArray
         }));
       }
     };
--- client/lib/modules/home-module/public/js/home.js
+++ client/lib/modules/home-module/public/js/home.js
@@ -2,13 +2,18 @@ $(document).ready(function () {
     new Vue({
         el: '#landing-page',
         data: {
-            location: window.location
+            location: window.location,
+            searchQuery: null,
+            filteredData: [],
+            showSearchResults: false,
+            resources: [],
         },
         beforeMount: function () {
             $('#loader').show();
         },
 
         mounted: function () {
+            this.resources = JSON.parse(document.getElementById('resources').value)
             setTimeout(function () {
                 $('#loader').hide();
             }, 1000);
@@ -25,7 +30,22 @@ $(document).ready(function () {
                 theme = $node.getAttribute('data-theme');
                 $('.theme-wrapper').removeClass('net default small large').addClass('net ' + theme).addClass('body-bg');
             },
-
+            filterPieces: function () {
+                if (this.searchQuery) {
+                    this.filteredData = [];
+                    this.showSearchResults = true;
+                    return this.resources.filter((item) => {
+                        // TODO: add description and other content after CMS
+                        if (this.searchQuery.toLowerCase().split(' ').every(v => item.title.toLowerCase().includes(v))) {
+                            this.filteredData.push(item)
+                        }
+                        return this.filteredData
+                    })
+                } else {
+                    this.showSearchResults = false;
+                    return this.filteredData = [];
+                }
+            }
         }
 
     })
