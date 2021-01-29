40d96404dfe2b1d7b30bb030d474646c6d35312e
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-01-29 19:14:09 +0530

    wip-text size logic

---
+++ client/lib/modules/accessibility-module/public/js/textChange.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var current = 16;
+    $('.resize_range').on('change', function () {
+        debugger
+        console.log(" vacurrentlue", $(this).val())
+        var change = $(this).val();
+        if (Number(change) >= 16) {
+            console.log(current, "current value")
+            if (Number(change) >= Number(current)) {
+                var inc = Number(change) - Number(current);
+                console.log('---inc', inc)
+                $('p,h1,h2,h3,h4,h5,label,span,button,input,a').each(function (res) {
+                    var fontsize = parseInt($(this).css('font-size'));
+                    var newFontsize = (fontsize + inc) + 'px';
+                    $(this).css('font-size', newFontsize);
+                });
+                console.log('if change', change)
+                console.log('if current', current)
+                current = change;
+            }
+            else {
+                var dec = Number(current) - Number(change);
+                console.log('dec---', dec)
+                $('p,h1,h2,h3,h4,h5,label,span,button,input,a').each(function (res) {
+                    var fontsize = parseInt($(this).css('font-size'));
+                    var newFontsize = (fontsize - dec) + 'px';
+                    $(this).css('font-size', newFontsize);
+                });
+                console.log('else change', change)
+                console.log('else current', current)
+                current = change;
+            }
+        }
+    });
+});
