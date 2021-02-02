6527ad143a55fca370e0fbac3c28570ff1184316
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-02-02 18:57:32 +0530

    wip-dark theme logic

--- client/lib/modules/accessibility-module/public/js/theme.js
+++ client/lib/modules/accessibility-module/public/js/theme.js
@@ -2,21 +2,43 @@ $(document).ready(function () {
     var app = new Vue({
         el: '#themePage',
         data: {
-            textSizeData: ''
+            theme: ''
         },
         beforeMount: function () {
             $('#loader').show();
         },
 
         mounted: function () {
+            var switchElem = document.getElementById('switchId');
+            switchElem.setAttribute('checked', false);
+            if (localStorage.getItem('theme')) {
+                this.theme = localStorage.getItem('theme');
+            }
             setTimeout(function () {
                 $('#loader').hide();
             }, 1000);
         },
 
         methods: {
-            changeTextSize: function () {
-                console.log('clicked');
+
+            changeTheme: function (e) {
+                if (e.target.checked) {
+                    document.documentElement.setAttribute('data-theme', 'dark');
+                    localStorage.setItem('theme', 'dark');
+                }
+                else {
+                    document.documentElement.setAttribute('data-theme', 'light');
+                    localStorage.setItem('theme', 'light');
+                }
+            },
+
+            saveTheme: function (selectedTheme) {
+                localStorage.setItem('theme', selectedTheme);
+                $('#themeSuccessModal').modal('show');
+            },
+
+            closeModal: function () {
+                $('#themeSuccessModal').modal('hide');
             }
         }
 
