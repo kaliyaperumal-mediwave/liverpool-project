3be508a7557f5fba83b0e17a28e8745000c014b6
sumithra16 <sumithra@mindwaveventures.com>
2021-02-11 11:08:11 +0530

    [FIX] added sort function and apostrophe fix

--- client/lib/modules/liverpool-mental-health/index.js
+++ client/lib/modules/liverpool-mental-health/index.js
@@ -1,56 +1,68 @@
 module.exports = {
-  extend: 'apostrophe-pieces',
-  name: 'About the people',
-  label: 'About the people',
-  pluralLabel: 'About the people',
+  extend: "apostrophe-pieces",
+  name: "About the people",
+  label: "About the people",
+  pluralLabel: "About the people",
   addFields: [
     {
-      name: 'Topic',
-      label: 'Topic',
-      type: 'string',
-      def: 'People',
-      readOnly: true
+      name: "Topic",
+      label: "Topic",
+      type: "string",
+      def: "People",
+      readOnly: true,
     },
     {
-      name: 'title',
-      label: 'Title',
-      type: 'string',
-      required: true
-    },
-    {
-      name: 'subtitle',
-      label: 'Sub Title',
-      type: 'string'
-      //required: true
-    },
-    {
-      name: 'text',
-      label: 'Text',
-      type: 'string',
+      name: "title",
+      label: "Title",
+      type: "string",
       required: true,
-      textarea: true
     },
     {
-    name: 'thumbnail',
-    label: 'Thumbnail',
-    type: 'singleton',
-    required: true,
-    widgetType: 'apostrophe-images',
-    options: {
-      limit: [ 1 ]
-    }
-  }
+      name: "subtitle",
+      label: "Sub Title",
+      type: "area",
+      options: {
+        widgets: {
+          "apostrophe-rich-text": {
+            toolbar: ["Bold", "Italic", "Link", "Unlink"],
+          },
+        },
+      },
+    },
+    {
+      name: "text",
+      label: "Text",
+      type: "area",
+      options: {
+        widgets: {
+          "apostrophe-rich-text": {
+            toolbar: ["Bold", "Italic", "Link", "Unlink"],
+          },
+        },
+      },
+      required: true,
+    },
+    {
+      name: "thumbnail",
+      label: "Thumbnail",
+      type: "singleton",
+      required: true,
+      widgetType: "apostrophe-images",
+      options: {
+        limit: [1],
+      },
+    },
   ],
   arrangeFields: [
     {
-      name: 'title',
-      label: 'Title',
-      fields: ['Topic', 'title','subtitle','text', 'tags','thumbnail']
+      name: "title",
+      label: "Title",
+      fields: ["Topic", "title", "subtitle", "text", "tags", "thumbnail"],
     },
     {
-      name: 'admin',
-      label: 'Administrative',
-      fields: ['slug', 'published']
-    }
-  ]
+      name: "admin",
+      label: "Administrative",
+      fields: ["slug", "published"],
+    },
+  ],
 };
