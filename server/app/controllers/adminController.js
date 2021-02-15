cb7bccd22df8048f00e1e9296c7762acaec2f491
kathirprabhakaran venkatesan <kathir@mindwaveventures.com>
2021-02-15 16:26:49 +0530

    [fix] display gp location based on the documentation

--- server/app/controllers/adminController.js
+++ server/app/controllers/adminController.js
@@ -1,7 +1,17 @@
+const moment = require('moment');
+const _ = require('lodash');
 const sequalizeErrorHandler = require('../middlewares/errorHandler');
 const reponseMessages = require('../middlewares/responseMessage');
 const Op = require('sequelize').Op;
-const moment = require('moment');
+
+const gpCodes = [
+    {
+        type: "Liverpool",
+        code: ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9", "L10", "L11", "L12", "L13", "L14", "L15",
+            "L16", "L17", "L18", "L19", "L20", "L24", "L25", "L26", "L27", "L28", "L32", "L33", "L34", "L35", "L36", "PR8", "PR9"]
+    },
+    {type: "Sefton", code: ["L21", "L22", "L23", "L29", "L30", "L31", "L37", "L38"]},
+]
 
 exports.getReferral = ctx => {
     return new Promise(async (resolve, reject) => {
@@ -35,48 +45,50 @@ exports.getReferral = ctx => {
             });
 
             referrals = JSON.parse(JSON.stringify(referrals));
-            let referralsObj = [];
-            let referrals_length = referrals.length;
-            for (let index = 0; index < referrals_length; index++) {
-                let referralObj = {
-                    uuid: referrals[index].uuid,
+            _.forEach(referrals, function(refObj, index) {
+                var referralObj = {
+                    uuid: refObj.uuid,
                     name: '',
                     dob: '',
-                    reference_code: referrals[index].reference_code,
+                    reference_code: refObj.reference_code,
                     referrer: '',
-                    gp_location: 'Liverpool',
-                    referrer_type: referrals[index].user_role.charAt(0).toUpperCase() + referrals[index].user_role.slice(1),
-                    date: moment(referrals[index].updatedAt).format('DD/MM/YYYY')
+                    gp_location: '',
+                    referrer_type: refObj.user_role.charAt(0).toUpperCase() + refObj.user_role.slice(1),
+                    date: moment(refObj.updatedAt).format('DD/MM/YYYY')
                 }
-                let gp_location = '';
-                if (referrals[index].user_role.toLowerCase() == 'child') {
-                    referralObj.name = referrals[index].child_name;
-                    referralObj.dob = moment(referrals[index].child_dob).format('DD/MM/YYYY');
-                    referralObj.referrer = referrals[index].child_name;
-                    gp_location = referrals[index].registerd_gp;
-                } else if ((referrals[index].user_role.toLowerCase() == 'parent') && referrals[index].parent && referrals[index].parent.length) {
-                    referralObj.name = referrals[index].parent[0].child_name;
-                    referralObj.dob = moment(referrals[index].parent[0].child_dob).format('DD/MM/YYYY');
-                    referralObj.referrer = referrals[index].parent_name;
-                    gp_location = referrals[index].parent[0].registerd_gp;
-                } else if ((referrals[index].user_role.toLowerCase() == 'professional') && referrals[index].professional && referrals[index].professional.length) {
-                    referralObj.name = referrals[index].professional[0].child_name;
-                    referralObj.dob = moment(referrals[index].professional[0].child_dob).format('DD/MM/YYYY');
-                    referralObj.referrer = referrals[index].professional_name;
-                    gp_location = referrals[index].professional[0].registerd_gp;
+                var gp_location = '';
+                if (refObj.user_role.toLowerCase() == 'child') {
+                    referralObj.name = refObj.child_name;
+                    referralObj.dob = moment(refObj.child_dob).format('DD/MM/YYYY');
+                    referralObj.referrer = refObj.child_name;
+                    gp_location = refObj.registerd_gp;
+                } else if ((refObj.user_role.toLowerCase() == 'parent') && refObj.parent && refObj.parent.length) {
+                    referralObj.name = refObj.parent[0].child_name;
+                    referralObj.dob = moment(refObj.parent[0].child_dob).format('DD/MM/YYYY');
+                    referralObj.referrer = refObj.parent_name;
+                    gp_location = refObj.parent[0].registerd_gp;
+                } else if ((refObj.user_role.toLowerCase() == 'professional') && refObj.professional && refObj.professional.length) {
+                    referralObj.name = refObj.professional[0].child_name;
+                    referralObj.dob = moment(refObj.professional[0].child_dob).format('DD/MM/YYYY');
+                    referralObj.referrer = refObj.professional_name;
+                    gp_location = refObj.professional[0].registerd_gp;
                 }
                 if(gp_location) {
                     var splitLocation = gp_location.split(',');
-                    if(splitLocation> 1) {
-                        referralObj.gp_location = (splitLocation[1][0].toUpperCase() == 'S') ? 'Sefton' : 'Liverpool';
+                    if(splitLocation.length > 1) {
+                        if(gpCodes[0].code.indexOf(splitLocation[1].split(' ')[0]) >= 0) {
+                            referralObj.gp_location = "Liverpool";
+                        } else if(gpCodes[1].code.indexOf(splitLocation[1].split(' ')[0]) >= 0) {
+                            referralObj.gp_location = "Sefton";
+                        }
                     }
                 }
-                referralsObj.push(referralObj);
-            }
+                referrals[index] = referralObj;
+            });
 
             resolve(
                 ctx.res.ok({
-                    data: referralsObj
+                    data: referrals
                 })
             );
         } catch (error) {
