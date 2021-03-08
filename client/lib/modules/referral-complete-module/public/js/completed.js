5c9c3799a63f51dec58c5210de8da12ab23673ac
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-03-08 18:50:00 +0530

    feat-completed F.E logic with mock data and mock service integration logic

--- client/lib/modules/referral-complete-module/public/js/completed.js
+++ client/lib/modules/referral-complete-module/public/js/completed.js
@@ -18,19 +18,48 @@ $(document).ready(function () {
             isFormSubmitted: false,
             showVisibilityPassword: false,
             showVisibilityConfirmPassword: false,
-            sendObj: {}
+            sendObj: {},
+            showSignUpForm: false,
+            isEmailRequired: false,
+            emailRegex: /^[a-z-0-9_+.-]+\@([a-z0-9-]+\.)+[a-z0-9]{2,7}$/i,
+            passwordRegex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&?*-])\S{7,}.$/,
         },
         beforeMount: function () {
             $('#loader').show();
         },
 
         mounted: function () {
+            var isLoggedInUser = document.getElementById('logId').innerHTML; // hide in layout.html
+            if (!isLoggedInUser) {
+                this.showSignUpForm = true;
+                //this.getSignUpData();
+            } else {
+                this.showSignUpForm = false;
+            }
             this.paramValues = getParameter(location.href)
             this.loginFlag = document.getElementById('uRole').innerHTML; // hide in layout.html
             this.getRefNo();
         },
 
         methods: {
+
+            getSignUpData: function () {
+                var successData = apiCallGet('get', '/your api call', API_URI);
+                if (successData && Object.keys(successData)) {
+                    Vue.set(this.refSignUpData, "role", successData.data.role);
+                    if (successData.data.email) {
+                        Vue.set(this.refSignUpData, "email", successData.data.email);
+                        document.getElementById('refEmail').setAttribute('readonly', true);
+                        this.isEmailRequired = true;
+                    } else {
+                        document.getElementById('refEmail').setAttribute('readonly', false);
+                        this.isEmailRequired = false;
+                    }
+                    $('#loader').hide();
+                } else {
+                    $('#loader').hide();
+                }
+            },
 
             //Function to trim space entered
             trimWhiteSpace: function (event, obj, key) {
@@ -62,6 +91,46 @@ $(document).ready(function () {
                         showError(error.responseJSON.message, error.status);
                     }
                 });
+            },
+
+            noLoginSignUp: function () {
+                console.log('clicked');
+                let formData = this.refSignUpData;
+                this.isFormSubmitted = true;
+                if (this.isEmailRequired) {
+                    if ((formData.password && this.passwordRegex.test(formData.password) && formData.confirmPassword && this.passwordRegex.test(formData.confirmPassword) && (formData.password === formData.confirm_password))) {
+                        $('#loader').show();
+                        var successData = apiCallPost('post', '/doCreateAcc', formData);
+                        if (successData && Object.keys(successData)) {
+                            this.tokenVariable = successData;
+                            $('#loader').hide();
+                            $('#signInSuccess').modal('show');
+
+                        } else {
+                            $('#loader').hide();
+                        }
+                    } else {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+                } else {
+                    if ((formData.email && this.emailRegex.test(formData.email) && formData.password && this.passwordRegex.test(formData.password) && formData.confirmPassword && this.passwordRegex.test(formData.confirmPassword) && (formData.password === formData.confirm_password))) {
+                        $('#loader').show();
+                        var successData = apiCallPost('post', '/doCreateAcc', formData);
+                        if (successData && Object.keys(successData)) {
+                            this.tokenVariable = successData;
+                            $('#loader').hide();
+                            $('#signInSuccess').modal('show');
+
+                        } else {
+                            $('#loader').hide();
+                        }
+                    } else {
+                        scrollToInvalidInput();
+                        return false;
+                    }
+                }
+
             },
 
             sendMail: function (payLoadObj) {
