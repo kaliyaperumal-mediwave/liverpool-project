cdcf28241e6f33b03e2b00e5e46d0c01eefc6803
sumithra16 <sumithra@mindwaveventures.com>
2021-03-09 12:04:23 +0530

    [ADD] events page UI and functionality work

--- client/lib/modules/liverpool-events-pages/index.js
+++ client/lib/modules/liverpool-events-pages/index.js
@@ -1,54 +1,110 @@
-var _ = require('lodash');
-const moment = require('moment');
+var _ = require("lodash");
+const moment = require("moment");
 
 module.exports = {
-  name: 'liverpool-events-pages',
-  extend: 'apostrophe-pieces-pages',
-  label: 'eventPage',
-  piecesFilters: [
-    { name: 'tags' }
-  ],
-
+  name: "liverpool-events-pages",
+  extend: "apostrophe-pieces-pages",
+  label: "eventPage",
+  piecesFilters: [{ name: "tags" }],
 
   construct: function (self, options) {
     var superBefore = self.beforeShow;
     self.beforeShow = function (req, callback) {
-      require('../../middleware')(self, options);
+      delete ($el, value);
+      console.log("--------", +$el);
 
-      self.checkCommonPageAuth(req).then((req) => {
-        return superBefore(req, callback);
-      }).catch(() => {
-      });
+      require("../../middleware")(self, options);
+
+      self
+        .checkCommonPageAuth(req)
+        .then((req) => {
+          return superBefore(req, callback);
+        })
+        .catch(() => {});
     };
     var beforeIndex = self.beforeIndex;
     self.beforeIndex = async function (req, callback) {
-      require('../../middleware')(self, options);
+      require("../../middleware")(self, options);
       req.data.piecesArray = _.map(req.session.eventsArray, (item) => {
-        item.custom_url = "/events?piece_id=" + item._id
+        item.custom_url = "/events?piece_id=" + item._id;
         return item;
-      })
+      });
       console.log(req.session.eventsArrayArray);
 
       const pieces = [];
-        for (let index = 0; index < req.session.eventsArray.length; index++) {
-          if(req.session.eventsArray[index].createdAt) {
-            req.session.eventsArray[index].uploadTime = moment(req.session.eventsArray[index].createdAt).fromNow();
+      const today = moment().format("YYYY-MM-DD hh:mm:ss");
+      for (let index = 0; index < req.session.eventsArray.length; index++) {
+        req.session.eventsArray[index].piece_date = moment(
+          req.session.eventsArray[index].date
+        ).format("dddd Do MMMM YYYY"); // Tuesday 23rd February 2021
+        req.session.eventsArray[index].start_time = moment(
+          req.session.eventsArray[index].starttime,
+          "HH:mm"
+        ).format("hh:mma");
+
+        req.session.eventsArray[index].end_time = moment(
+          req.session.eventsArray[index].endtime,
+          "HH:mm"
+        ).format("hh:mma");
+
+        const start_date =
+          req.session.eventsArray[index].date +
+          " " +
+          req.session.eventsArray[index].starttime;
+        const diff = moment(start_date, "YYYY-MM-DD hh:mm:ss").diff(
+          moment(today, "YYYY-MM-DD hh:mm:ss"),
+          "days",
+          true
+        );
+        if (diff >= 0 && diff < 1) {
+          const end_date =
+            req.session.eventsArray[index].date +
+            " " +
+            req.session.eventsArray[index].endtime;
+          if (
+            moment(moment(today, "YYYY-MM-DD hh:mm:ss")).isBetween(
+              moment(start_date, "YYYY-MM-DD hh:mm:ss"),
+              moment(end_date, "YYYY-MM-DD hh:mm:ss")
+            )
+          ) {
+            req.session.eventsArray[index].uploadTime =
+              "On going end by " + req.session.eventsArray[index].end_time;
           } else {
-            req.session.eventsArray[index].uploadTime = '';
+            req.session.eventsArray[index].uploadTime =
+              "Today at " + req.session.eventsArray[index].start_time;
           }
-          if (req.query && req.query.piece_id) {
-            if (req.session.eventsArray[index]._id == req.query.piece_id) {
-              pieces.splice(0, 0, req.session.eventsArray[index]);
-            } else {
-              pieces.push(req.session.eventsArray[index]);
-            }
+        } else if (diff >= 1 && diff < 2) {
+          req.session.eventsArray[index].uploadTime =
+            "Tomorrow at " + req.session.eventsArray[index].start_time;
+        } else if (diff >= 2) {
+          req.session.eventsArray[index].uploadTime =
+            moment(req.session.eventsArray[index].date, "YYYY-MM-DD").format(
+              "DD/MM/YYYY"
+            ) +
+            " at " +
+            req.session.eventsArray[index].start_time;
+        }
+
+        console.log("\n", req.session.eventsArray[index].title, "   ", diff);
+
+        //   req.session.eventsArray[index].uploadTime = moment(
+        //     req.session.eventsArray[index].createdAt
+        //   ).fromNow();
+        if (req.query && req.query.piece_id && diff >= 0) {
+          if (req.session.eventsArray[index]._id == req.query.piece_id) {
+            pieces.splice(0, 0, req.session.eventsArray[index]);
+          } else {
+            pieces.push(req.session.eventsArray[index]);
           }
         }
-        req.data.pieces = pieces;
-      self.checkCommonPageAuth(req).then((req) => {
-        return beforeIndex(req, callback);
-      }).catch(() => {
-      });
+      }
+      req.data.pieces = pieces;
+      self
+        .checkCommonPageAuth(req)
+        .then((req) => {
+          return beforeIndex(req, callback);
+        })
+        .catch(() => {});
     };
-  }
+  },
 };
--- client/lib/modules/liverpool-events/index.js
+++ client/lib/modules/liverpool-events/index.js
@@ -1,54 +1,124 @@
 module.exports = {
-  extend: 'apostrophe-pieces',
-  name: 'eventPage',
-  label: 'Event',
-  pluralLabel: 'Events',
+  extend: "apostrophe-pieces",
+  name: "eventPage",
+  label: "Event",
+  pluralLabel: "Events",
   addFields: [
     {
-      name: 'Topic',
-      label: 'Topic',
-      type: 'string',
-      def: 'Events',
-      readOnly: true
-    },{
-      name: 'title',
-      label: 'Title',
-      type: 'string',
+      name: "Topic",
+      label: "Topic",
+      type: "string",
+      def: "Events",
+      readOnly: true,
+    },
+    {
+      name: "title",
+      label: "Title",
+      type: "string",
       required: true,
     },
     {
-    name: 'thumbnail',
-    label: 'Thumbnail',
-    type: 'singleton',
-    widgetType: 'apostrophe-images',
-    options: {
-      limit: [ 1 ]
-    }
-  },
-    {
-      name: 'eventsTopic',
-      label: 'Events_Topic',
-      type: 'string',
-      textarea: true
+      name: "thumbnail",
+      label: "Thumbnail",
+      type: "singleton",
+      widgetType: "apostrophe-images",
+      options: {
+        limit: [1],
+      },
     },
     {
-    name: 'text',
-    label: 'Text',
-    type: 'string',
-    required: true,
-    textarea: true
-  }
+      name: "eventsTopic",
+      label: "Events_Topic",
+      type: "string",
+      textarea: true,
+    },
+    {
+      name: "text",
+      label: "Text",
+      type: "string",
+      required: true,
+      textarea: true,
+    },
+    {
+      name: "joinLink",
+      label: "Join Event URL",
+      type: "url",
+      required: true,
+    },
+    {
+      name: "location",
+      label: "Location",
+      type: "string",
+      required: true,
+    },
+    // {
+    //   name: 'latitude',
+    //   label: 'Latitude',
+    //   type: 'string',
+    //   required: true
+    // },
+    // {
+    //   name: 'landitude',
+    //   label: 'Landitude',
+    //   type: 'string',
+    //   required: true
+    // },
 
-  ],
-  arrangeFields: [{
-      name: 'events',
-      label: 'Events',
-      fields: ['Topic','title','thumbnail','eventsTopic','text']
+    {
+      name: "date",
+      label: "Date",
+      type: "date",
+      def: null,
+      required: true,
+      pikadayOptions: {
+        showTime: true,
+        format: "LLLL",
+        firstDay: 1,
+      },
     },
     {
-      name: 'admins',
-      label: 'Administratives',
-      fields: [ 'slug','published','tags']
-    }
-  ]
+      name: "starttime",
+      label: "StartTime",
+      type: "time",
+      def: null,
+      required: true,
+      pikadayOptions: {
+        showTime: true,
+        format: "HH:MM:SS",
+        firstDay: 1,
+      },
+    },
+    {
+      name: "endtime",
+      label: "EndTime",
+      type: "time",
+      def: null,
+      required: true,
+      pikadayOptions: {
+        showTime: true,
+        format: "HH:MM:SS",
+        firstDay: 1,
+      },
+    },
+  ],
+  arrangeFields: [
+    {
+      name: "events",
+      label: "Events",
+      fields: [
+        "Topic",
+        "title",
+        "thumbnail",
+        "eventsTopic",
+        "text",
+        "location",
+        "joinLink",
+      ],
+    },
+    {
+      name: "admins",
+      label: "Administratives",
+      fields: ["date", "starttime", "endtime", "slug", "published", "tags"],
+    },
+  ],
 };
