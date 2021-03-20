d5b1c3288bcfcf7ceb99a948cb9cd2d053397da5
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-03-20 14:30:06 +0530

    wip-orcha pagination

--- client/lib/modules/orcha-module/public/js/orcha-home.js
+++ client/lib/modules/orcha-module/public/js/orcha-home.js
@@ -1,18 +1,61 @@
+var API_URI = "/modules/orcha-module";
 $(document).ready(function () {
-    
+
     var app = new Vue({
-        el: '#orchaHomePage',
+        el: '#orchaNewHomePage',
+        components: {
+            Multiselect: window.VueMultiselect.default
+        },
         data: {
-            searchReferrals: [],
+            categoryValue: null,
+            countryValue: null,
+            capabilityValue: null,
+            designedForValue: null,
+            costValue: null,
+            platformValue: null,
+            checkBoxTest: null,
+            categoryList: [],
+            countryList: [],
+            capabilityList: [],
+            designedForList: [],
+            costList: [],
+            platformList: []
         },
 
+        beforeMount: function () {
+            $('#loader').show();
+        },
         mounted: function () {
+            this.getAllDataForDropdown();
             $('#loader').hide();
         },
 
         methods: {
-            toggleArrow: function (e) {
+            getAllDataForDropdown: function () {
+                var successData = apiCallGet('get', '/getFilterData/', API_URI);
+                if (successData && Object.keys(successData)) {
+                    console.log(successData);
+                    this.categoryList = successData.data.subCategory_payload;
+                    this.countryList = successData.data.country_payload;
+                    this.capabilityList = successData.data.capability_payload;
+                    this.designedForList = successData.data.designedFor_payload;
+                    this.costList = successData.data.cost_payload;
+                    this.platformList = successData.data.platform_payload;
+                } else {
+                    $('#loader').hide();
+                }
             },
+
+            selectOptions: function (e) {
+                debugger
+                console.log(e);
+            },
+
+            removeOptions: function (option) {
+                debugger
+                console.log("Removed", option);
+
+            }
         }
     })
 });
