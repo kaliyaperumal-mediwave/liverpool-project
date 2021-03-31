2fd5eb2e7d7dc6f5a5c789a58b2ab5ae68df8d56
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-03-31 22:33:03 +0530

    feat-typo changes

--- client/lib/modules/referral-home-module/public/js/referral-home.js
+++ client/lib/modules/referral-home-module/public/js/referral-home.js
@@ -10,13 +10,18 @@ $(document).ready(function () {
         },
 
         mounted: function () {
+            var isLoggedInUser = $('#loginUserFlag').text();
+            if (isLoggedInUser == 'false') {
+                $('#8b91a47a-018a-4916-8804-0c2b197fdaa1').removeClass().addClass('py-2 d-block');
+                $('#fcf6cdd9-aded-48ad-8476-406d46f3f007').removeClass().addClass('option-font px-3 text-left m-0 d-block');;
+            }
             setTimeout(function () {
                 $('#loader').hide();
             }, 1000);
         },
 
         methods: {
-          
+
             goToRole: function () {
                 var _self = this;
                 $.ajax({
@@ -27,13 +32,13 @@ $(document).ready(function () {
                     cache: false,
                     success: function (data) {
                         //console.log("-------------------------------success block");
-                       location.href = "/role";
-                      // window.top.location= "/role";
-                       // parent.location.href = "/role";
-                       if (window != window.top) { 
-                        // the page is inside an iframe
-                       // window.location.href = "/role"
-                    }
+                        location.href = "/role";
+                        // window.top.location= "/role";
+                        // parent.location.href = "/role";
+                        if (window != window.top) {
+                            // the page is inside an iframe
+                            // window.location.href = "/role"
+                        }
                     },
                     error: function (error) {
                         //console.log(error)
