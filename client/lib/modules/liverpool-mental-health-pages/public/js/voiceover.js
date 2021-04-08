1bb60c67f3fff6ddc3a15603248fc6e395b46703
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-04-08 19:49:24 +0530

    fix-voice over

--- client/lib/modules/liverpool-mental-health-pages/public/js/voiceover.js
+++ client/lib/modules/liverpool-mental-health-pages/public/js/voiceover.js
@@ -1,77 +1,44 @@
 
 $(document).ready(function () {
-    var isIE = false || !!document.documentMode;
-    if (!isIE) {
+    var userPlatform = getMobileOperatingSystem();
+    if (userPlatform == 'Android') {
+        alert(userPlatform);
         var ssu = new SpeechSynthesisUtterance();
         var myEvent = window.attachEvent || window.addEventListener;
-        var voiceAttachEvent = window.attachEvent ? 'onbeforeunload' : 'beforeunload';
+        $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none');
         if (localStorage.getItem('voiceOver') && localStorage.getItem('voiceOver') == 'on') {
-            $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-none').addClass('d-flex');
+            $('#androidContainer').removeClass('d-none').addClass('d-flex');
         } else if (localStorage.getItem('voiceOver') && localStorage.getItem('voiceOver') == 'off') {
-            $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none');
+            $('#androidContainer').removeClass('d-flex').addClass('d-none');
         } else {
-            $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none');
+            $('#androidContainer').removeClass('d-flex').addClass('d-none');
         }
         var title = $('#voiceOverTitle').text();
         title = title.trim();
         var description = $('#voiceOverDescription').text();
         description = description.trim();
-        // optimisation needed
-        // $("#playButton").hide();
 
-        // document.getElementById('resumeButton').classList.add('d-none');
-        // document.getElementById('playButton').classList.add('active-border');
-        $('#playButton').on('click', function (e) {
-            $('#stopButton').removeClass('active-border');
-            document.getElementById('playButton').classList.remove('active-border');
-            document.getElementById('stopButton').classList.add('active-border')
-            document.getElementById('stopButton').style.opacity = 1;
-            document.getElementById('stopButton').removeAttribute('disabled');
-            document.getElementById('playButton').setAttribute('disabled', true);
-            document.getElementById('playButton').classList.add('d-none');
-            document.getElementById('pauseButton').classList.remove('d-none')
-
-            // $("#playButton").hide();
+        $('#playAndroidButton').on('click', function (e) {
+            $('#stopAndroidButton').removeClass('active-border');
+            document.getElementById('playAndroidButton').classList.remove('active-border');
+            document.getElementById('stopAndroidButton').classList.add('active-border')
+            document.getElementById('stopAndroidButton').style.opacity = 1;
+            document.getElementById('stopAndroidButton').removeAttribute('disabled');
+            document.getElementById('playAndroidButton').setAttribute('disabled', true);
+            document.getElementById('playAndroidButton').classList.add('d-none');
             wrapper();
         });
 
-        $('#stopButton').on('click', function (e) {
-            document.getElementById('playButton').classList.add('active-border');
-            document.getElementById('stopButton').classList.remove('active-border')
-            document.getElementById('stopButton').setAttribute('disabled', true);
-            document.getElementById('stopButton').style.opacity = 0.4;
-            document.getElementById('playButton').removeAttribute('disabled');
-            document.getElementById('resumeButton').classList.add('d-none');
-            document.getElementById('pauseButton').classList.add('d-none');
-            document.getElementById('playButton').classList.remove('d-none');
-            $("#playButton").show();
-            // $("#playButton").hide();
-
+        $('#stopAndroidButton').on('click', function (e) {
+            document.getElementById('playAndroidButton').classList.add('active-border');
+            document.getElementById('stopAndroidButton').classList.remove('active-border')
+            document.getElementById('stopAndroidButton').setAttribute('disabled', true);
+            document.getElementById('stopAndroidButton').style.opacity = 0.4;
+            document.getElementById('playAndroidButton').removeAttribute('disabled');
+            document.getElementById('playAndroidButton').classList.remove('d-none');
+            $("#playAndroidButton").show();
             wrapper(true);
         });
-
-        $('#pauseButton').on('click', function (e) {
-            document.getElementById('resumeButton').classList.remove('d-none');
-            document.getElementById('pauseButton').classList.add('d-none')
-            document.getElementById('stopButton').classList.add('active-border')
-            document.getElementById('stopButton').style.opacity = 1;
-            document.getElementById('stopButton').removeAttribute('disabled');
-            $("#playButton").hide();
-
-            window.speechSynthesis.pause();
-
-        });
-        $('#resumeButton').on('click', function (e) {
-            document.getElementById('resumeButton').classList.add('d-none');
-            document.getElementById('pauseButton').classList.remove('d-none');
-            document.getElementById('stopButton').classList.add('active-border')
-            document.getElementById('stopButton').style.opacity = 1;
-            document.getElementById('stopButton').removeAttribute('disabled');
-            $("#playButton").hide();
-
-            window.speechSynthesis.resume();
-        });
-        var ssu = new SpeechSynthesisUtterance();
         function wrapper(stopFlag) {
             if (stopFlag) {
                 ssu.text = ""
@@ -85,51 +52,13 @@ $(document).ready(function () {
             window.speechSynthesis.speak(ssu);
         }
         ssu.onend = function (e) {
-            document.getElementById('playButton').classList.add('active-border');
-            document.getElementById('stopButton').classList.remove('active-border')
-            document.getElementById('stopButton').setAttribute('disabled', true);
-            document.getElementById('stopButton').style.opacity = 0.4;
-            document.getElementById('playButton').removeAttribute('disabled');
-            document.getElementById('playButton').classList.remove('d-none');
-            document.getElementById('pauseButton').classList.add('d-none');
+            document.getElementById('playAndroidButton').classList.add('active-border');
+            document.getElementById('stopAndroidButton').classList.remove('active-border')
+            document.getElementById('stopAndroidButton').setAttribute('disabled', true);
+            document.getElementById('stopAndroidButton').style.opacity = 0.4;
+            document.getElementById('playAndroidButton').removeAttribute('disabled');
+            document.getElementById('playAndroidButton').classList.remove('d-none');
         };
-        // ssu.onend = function (e) {
-        //     console.log('Finished in ' + e.elapsedTime + ' seconds.');
-        // };
-        // async function wrapper(stopFlag) {
-        //     console.log("called========");
-        //     var text = description;
-        //     var result = text.match(/[^\.!\?]+[\.!\?]+/g);
-        //     console.log(result.length, "result.length");
-        //     for (var i = 0; i < result.length; i++) {
-        //         sentence = result[i];
-        //         ssu.text = sentence;
-        //         ssu.lang = "en-US";
-        //         await new Promise(function (resolve) {
-        //             if (stopFlag) {
-        //                 ssu.text = ""
-        //                 window.speechSynthesis.cancel();
-        //                 window.speechSynthesis.speak(ssu);
-        //                 return false;
-        //             } else {
-        //                 window.speechSynthesis.cancel();
-        //                 window.speechSynthesis.speak(ssu);
-        //             }
-        //         });
-
-        //     }
-        // }
-
-
-        // $(window).on('beforeunload', function () {
-        //     console.log(" page end");
-        //     ssu.text = ""
-        //     window.speechSynthesis.cancel();
-        //     window.speechSynthesis.speak(ssu);
-        //     return false;
-        //     // window.speechSynthesis.pause();
-        //     // wrapper(true)
-        // });
 
         myEvent(voiceAttachEvent, function (e) {
             window.speechSynthesis.cancel();
@@ -143,19 +72,130 @@ $(document).ready(function () {
             window.speechSynthesis.cancel();
         });
 
-        if ($('.checkEmpty').is(':visible')) {
-            $(".replaceClass").addClass('col-6 col-lg-9');
-            $(".replaceClass").removeClass('col-12');
-            $(".checkEmpty").show();
-        } else {
-            var replace = $(".replaceClass");
-            $(".replaceClass").removeClass('col-6 col-lg-9');
-            $(".replaceClass").addClass('col-12');
-            $(".checkEmpty").hide();
-        }
 
     } else {
-        $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none')
+        var isIE = false || !!document.documentMode;
+        if (!isIE) {
+            var ssu = new SpeechSynthesisUtterance();
+            var myEvent = window.attachEvent || window.addEventListener;
+            var voiceAttachEvent = window.attachEvent ? 'onbeforeunload' : 'beforeunload';
+            if (localStorage.getItem('voiceOver') && localStorage.getItem('voiceOver') == 'on') {
+                $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-none').addClass('d-flex');
+            } else if (localStorage.getItem('voiceOver') && localStorage.getItem('voiceOver') == 'off') {
+                $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none');
+            } else {
+                $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none');
+            }
+            var title = $('#voiceOverTitle').text();
+            title = title.trim();
+            var description = $('#voiceOverDescription').text();
+            description = description.trim();
+            // optimisation needed
+            // $("#playButton").hide();
+
+
+            $('#playButton').on('click', function (e) {
+                $('#stopButton').removeClass('active-border');
+                document.getElementById('playButton').classList.remove('active-border');
+                document.getElementById('stopButton').classList.add('active-border')
+                document.getElementById('stopButton').style.opacity = 1;
+                document.getElementById('stopButton').removeAttribute('disabled');
+                document.getElementById('playButton').setAttribute('disabled', true);
+                document.getElementById('playButton').classList.add('d-none');
+                document.getElementById('pauseButton').classList.remove('d-none')
+
+                // $("#playButton").hide();
+                wrapper();
+            });
+
+            $('#stopButton').on('click', function (e) {
+                document.getElementById('playButton').classList.add('active-border');
+                document.getElementById('stopButton').classList.remove('active-border')
+                document.getElementById('stopButton').setAttribute('disabled', true);
+                document.getElementById('stopButton').style.opacity = 0.4;
+                document.getElementById('playButton').removeAttribute('disabled');
+                document.getElementById('resumeButton').classList.add('d-none');
+                document.getElementById('pauseButton').classList.add('d-none');
+                document.getElementById('playButton').classList.remove('d-none');
+                $("#playButton").show();
+                // $("#playButton").hide();
+
+                wrapper(true);
+            });
+
+            $('#pauseButton').on('click', function (e) {
+                document.getElementById('resumeButton').classList.remove('d-none');
+                document.getElementById('pauseButton').classList.add('d-none')
+                document.getElementById('stopButton').classList.add('active-border')
+                document.getElementById('stopButton').style.opacity = 1;
+                document.getElementById('stopButton').removeAttribute('disabled');
+                $("#playButton").hide();
+
+                window.speechSynthesis.pause();
+
+            });
+            $('#resumeButton').on('click', function (e) {
+                document.getElementById('resumeButton').classList.add('d-none');
+                document.getElementById('pauseButton').classList.remove('d-none');
+                document.getElementById('stopButton').classList.add('active-border')
+                document.getElementById('stopButton').style.opacity = 1;
+                document.getElementById('stopButton').removeAttribute('disabled');
+                $("#playButton").hide();
+
+                window.speechSynthesis.resume();
+            });
+            var ssu = new SpeechSynthesisUtterance();
+            function wrapper(stopFlag) {
+                if (stopFlag) {
+                    ssu.text = ""
+                    window.speechSynthesis.cancel();
+                    window.speechSynthesis.speak(ssu);
+                    return false;
+                }
+                ssu.text = description;
+                ssu.lang = "en-US";
+                window.speechSynthesis.cancel();
+                window.speechSynthesis.speak(ssu);
+            }
+            ssu.onend = function (e) {
+                document.getElementById('playButton').classList.add('active-border');
+                document.getElementById('stopButton').classList.remove('active-border')
+                document.getElementById('stopButton').setAttribute('disabled', true);
+                document.getElementById('stopButton').style.opacity = 0.4;
+                document.getElementById('playButton').removeAttribute('disabled');
+                document.getElementById('playButton').classList.remove('d-none');
+                document.getElementById('pauseButton').classList.add('d-none');
+            };
+
+
+            myEvent(voiceAttachEvent, function (e) {
+                window.speechSynthesis.cancel();
+            });
+
+            var isOnIOS = navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i);
+            var eventName = isOnIOS ? "pagehide" : "beforeunload";
+
+            window.addEventListener(eventName, function (event) {
+                console.log(eventName, "eventNameeventName");
+                window.speechSynthesis.cancel();
+            });
+
+            if ($('.checkEmpty').is(':visible')) {
+                $(".replaceClass").addClass('col-6 col-lg-9');
+                $(".replaceClass").removeClass('col-12');
+                $(".checkEmpty").show();
+            } else {
+                var replace = $(".replaceClass");
+                $(".replaceClass").removeClass('col-6 col-lg-9');
+                $(".replaceClass").addClass('col-12');
+                $(".checkEmpty").hide();
+            }
+
+        } else {
+            $('#c730ed34-ce14-4df1-8718-6346cd050c2b').removeClass('d-flex').addClass('d-none')
+        }
+
     }
+
 
 });
