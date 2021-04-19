cc570ab34f2cf45cc4d329b09ed806ada52a696b
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-04-19 13:02:54 +0530

    fix-#855

--- server/app/middlewares/responseMessage.js
+++ server/app/middlewares/responseMessage.js
@@ -1,22 +1,22 @@
 const messageCodes = {
   1001: 'Updated successfully',
-  1002: 'Bad Request : Please provide valid data.',
+  1002: 'Bad Request : Please provide valid data',
   1003: 'Email already exists',
   1004: 'Invalid Email / Password',
   1005: 'User account created successfully',
   1006: 'User not found',
-  1007: 'Failed to update. Please try again.',
-  1008: 'You will receive a mail if you are registered at Liverpool CAMHS ',
-  1009: 'Link expired! Request again.',
-  1010: 'Password has been reset successfully! Try login with new password.',
+  1007: 'Failed to update. Please try again',
+  1008: 'You will receive a mail if you are registered at Liverpool CAMHS',
+  1009: 'Link expired! Request again',
+  1010: 'Password has been reset successfully! Try login with new password',
   1011: "Your old password doesn't match",
-  1012: 'That email is taken. Try another.',
-  1013: 'User not registered at Liverpool CAMHS ',
+  1012: 'That email is taken. Try another',
+  1013: 'User not registered at Liverpool CAMHS',
   1014: 'Thanks for your feedback!',
-  1015: 'Deleted successfully.',
-  1016: 'Data not found.',
+  1015: 'Deleted successfully',
+  1016: 'Data not found',
   1017: 'Referral details sent',
   1018: 'Unauthorized Access!',
-  1019: 'Invalid role for the user.'
+  1019: 'Invalid role for the user'
 };
 module.exports = messageCodes;
