a0320cb4e47b402aca977b3052bdcaeb902122cf
Thiru Prasath <thiru@mindwaveventures.com>
2021-04-19 12:26:14 +0530

    [wip] - 671

--- client/lib/modules/apostrophe-assets/public/js/utilities.js
+++ client/lib/modules/apostrophe-assets/public/js/utilities.js
@@ -683,3 +683,8 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function openApps() {
+    localStorage.removeItem("orFilData");
+    location.href = window.location.origin + '/apps';
+}
