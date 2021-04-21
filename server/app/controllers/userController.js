b702228d74cde7fbe9f848f2ea21f58618faaf17
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-04-21 14:44:02 +0530

    fix-typo mistakes

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -3,42 +3,42 @@ const saltRounds = 10;
 const sequalizeErrorHandler = require('../middlewares/errorHandler');
 const reponseMessages = require('../middlewares/responseMessage');
 
-exports.addAdminUsers = async(ctx) => {
+exports.addAdminUsers = async (ctx) => {
     console.log('ctx-----------', ctx.request.decryptedUser);
     console.log('ctx-----------', ctx.request.body);
 
     try {
 
-        if(ctx.request.decryptedUser.role !== 'admin'){
+        if (ctx.request.decryptedUser.role !== 'admin') {
             return ctx.res.badRequest({
                 message: reponseMessages[1018],
             });
         }
 
         const serviceAdminroles = [
-            'Alder Hey – Liverpool CAMHS & EDYS',
+            'Alder Hey - Liverpool CAMHS - EDYS',
             'YPAS',
             'MHST',
             'Seedlings',
             'Wellbeing Clinics',
-            'Alder Hey – Sefton CAMHS & EDYS',
+            'Alder Hey - Sefton CAMHS - EDYS',
             'Venus',
             'Parenting 2000',
             'Other'
         ]
-        
+
         const adminroles = [
             'admin',
             'service_admin'
         ]
 
-        if(!adminroles.includes(ctx.request.body.user_role)){
+        if (!adminroles.includes(ctx.request.body.user_role)) {
             return ctx.res.badRequest({
                 message: reponseMessages[1019],
             });
         }
-        
-        if(ctx.request.body.user_role === 'service_admin' && serviceAdminroles.includes(ctx.request.body.user_role)){
+
+        if (ctx.request.body.user_role === 'service_admin' && serviceAdminroles.includes(ctx.request.body.user_role)) {
             return ctx.res.badRequest({
                 message: reponseMessages[1019],
             });
@@ -68,13 +68,13 @@ exports.addAdminUsers = async(ctx) => {
             user_role: ctx.request.body.user_role,
         }
 
-        if(ctx.request.body.user_role === 'service_admin'){
+        if (ctx.request.body.user_role === 'service_admin') {
             userDetails.service_type = ctx.request.body.service_type;
         }
 
         const createUser = await user.create(userDetails);
 
-        if(createUser){
+        if (createUser) {
             return ctx.res.ok({
                 status: "success",
                 message: reponseMessages[1005],
