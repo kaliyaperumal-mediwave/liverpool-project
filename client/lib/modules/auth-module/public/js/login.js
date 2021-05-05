93d82068345cf1d0f8de17738617d96214d44499
priyanka246 <priyanka@mindwaveventures.com>
2021-05-05 21:55:29 +0530

    Revert "feat-role to a new tab"
    This reverts commit 18bf492ba02c7b829008af7925f46d75cb34bc48.

--- client/lib/modules/auth-module/public/js/login.js
+++ client/lib/modules/auth-module/public/js/login.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
         },
 
         mounted: function () {
+            //console.log(document.getElementById('sessionExp').innerHTML)
             var loginButton = document.getElementById('secondary');
             loginButton.removeAttribute('disabled');
             loginButton.style.opacity = 1;
@@ -35,6 +36,7 @@ $(document).ready(function () {
                         showError('Session expired');
                     },
                     error: function (error) {
+                        // console.log(error)
                         showError(error.responseJSON.message, error.status);
                     }
                 })
