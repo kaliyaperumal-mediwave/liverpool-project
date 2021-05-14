7ab056079788dfdaf3eaee913dbaf2f596d78ba0
ThiruPrasath <thiru@mindwaveventures.com>
2021-05-14 17:38:09 +0530

    [fix] - mail issue

--- client/lib/modules/review-module/public/js/review.js
+++ client/lib/modules/review-module/public/js/review.js
@@ -220,7 +220,7 @@ $(document).ready(function () {
                     if (this.contact_person && this.contactPref.length) {
                         $('#loader').show();
                         if (this.section1Data.gp_school != "" && this.section1Data.gp_school != null) {
-                            if(this.section1Data.service_location === 'liverpool'){
+                            if(this.section1Data.gp_school === 'Liverpool'){
                                 this.payloadData.referral_provider = "MHST Liverpool";
                             } else {
                                 this.payloadData.referral_provider = "MHST Sefton";
@@ -269,7 +269,7 @@ $(document).ready(function () {
                     if (this.contact_person && this.contactPref.length) {
                         $('#loader').show();
                         if (this.section1Data.gp_school != "" && this.section1Data.gp_school != null) {
-                            if(this.section1Data.service_location === 'liverpool'){
+                            if(this.section1Data.gp_school === 'Liverpool'){
                                 this.payloadData.referral_provider = "MHST Liverpool";
                             } else {
                                 this.payloadData.referral_provider = "MHST Sefton";
