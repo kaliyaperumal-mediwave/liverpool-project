c2d4da5635f1133499a69e0ee894712da0ece532
ThiruPrasath <thiru@mindwaveventures.com>
2021-06-07 17:19:20 +0530

    [wip] - wrike id - #689805700

--- server/app/utils/email.js
+++ server/app/utils/email.js
@@ -10,6 +10,8 @@ const logger = require('../logger');
 const pdf = require('../utils/pdfgenerate');
 sgMail.setApiKey(config.sendgrid_api_key);
 const reponseMessages = require('../middlewares/responseMessage');
+const { parse } = require('json2csv');
+const moment = require('moment');
 let mailService;
 // Sendgrid disabled on SMTP requirement
 if (process.env.USE_SENDGRID == 'true') {
@@ -143,7 +145,7 @@ exports.sendReferralConfirmationMail = async ctx => new Promise((resolve, reject
     try {
         if (ctx.request.decryptedUser != undefined) {
             const data = {
-                from:  config.email_from_address,
+                from: config.email_from_address,
                 to: ctx.request.decryptedUser.email,
                 subject: 'Referral Confirmation',
                 html: '<p> Your referral code is <strong>' + ctx.request.body.ref_code + '</strong><p>',
@@ -178,7 +180,7 @@ exports.sendReferralConfirmationMail = async ctx => new Promise((resolve, reject
 exports.sendReferralWithData = async ctx => new Promise((resolve, reject) => {
     var toAddress;
     try {
-        if (ctx.request.body.emailToProvider == "Alder Hey - Liverpool CAMHS - EDYS"){
+        if (ctx.request.body.emailToProvider == "Alder Hey - Liverpool CAMHS - EDYS") {
             toAddress = config.alder_hey_liverpol
         } else if (ctx.request.body.emailToProvider == "YPAS") {
             toAddress = config.ypas_email
@@ -199,7 +201,7 @@ exports.sendReferralWithData = async ctx => new Promise((resolve, reject) => {
         } else if (ctx.request.body.emailToProvider == "IAPTUS") {
             toAddress = config.iaptus_email
         }
-         else {
+        else {
             toAddress = config.other_email
         }
         console.log('toAddress----------', toAddress);
@@ -210,15 +212,75 @@ exports.sendReferralWithData = async ctx => new Promise((resolve, reject) => {
         });
         return pdf.generatePdf(ctx).then((sendReferralStatus) => {
             if (sendReferralStatus) {
+                console.log(ctx.request.body.referralData.section4)
+                const csvHeader = ["Title", "Name"];
+                const dataCsv = [
+                    { //Section 1
+                        "I am a": ctx.request.body.referralData.role,
+                        "Interpreter needed": ctx.request.body.referralData.section1.need_interpreter,
+                        "Child's D.O.B:": moment(ctx.request.body.referralData.section1.child_dob).format('DD/MM/YYYY'),
+                        "Consent given": ctx.request.body.referralData.section1.consent_child,
+                        "GP": ctx.request.body.referralData.section1.registered_gp,
+                        //Section 2
+                        "NHS number": ctx.request.body.referralData.section2.child_NHS,
+                        "Title": ctx.request.body.referralData.section2.child_name_title,
+                        "First name": ctx.request.body.referralData.section2.child_name,
+                        "Last name": ctx.request.body.referralData.section2.child_lastname,
+                        "E-mail:": ctx.request.body.referralData.section2.child_email,
+                        "Contact number": ctx.request.body.referralData.section2.child_contact_number,
+                        "Address": ctx.request.body.referralData.section2.child_address,
+                        "Happy for post to be send": ctx.request.body.referralData.section2.can_send_post,
+                        "Gender": ctx.request.body.referralData.section2.child_gender,
+                        "Sex at birth": ctx.request.body.referralData.section2.sex_at_birth,
+                        "Identifies with gender of birth": ctx.request.body.referralData.section2.child_gender_birth,
+                        "Sexual orientation": ctx.request.body.referralData.section2.child_sexual_orientation,
+                        "Ethnicity": ctx.request.body.referralData.section2.child_ethnicity,
+                        "Cares for an adult": ctx.request.body.referralData.section2.child_care_adult,
+                        "Parent / carer’s first name": ctx.request.body.referralData.section2.parent_name,
+                        "Parent / carer’s last name:": ctx.request.body.referralData.section2.parent_lastname,
+                        "Have a parental responsibility": ctx.request.body.referralData.section2.parental_responsibility,
+                        "Relationship": ctx.request.body.referralData.section2.child_parent_relationship,
+                        "Parent contact number": ctx.request.body.referralData.section2.parent_contact_number,
+                        "Parent email address": ctx.request.body.referralData.section2.parent_email,
+                        "Lives in same house": ctx.request.body.referralData.section2.parent_same_house,
+                        "Parent address": ctx.request.body.referralData.section2.parent_address,
+                        //Section3
+                        "Education / employment": ctx.request.body.referralData.section3.child_profession,
+                        "EHCP plan": ctx.request.body.referralData.section3.child_EHCP,
+                        "Open to EHAT": ctx.request.body.referralData.section3.child_EHAT,
+                        "Social worker": ctx.request.body.referralData.section3.child_socialworker,
+                        //section4
+                        "Support needs": ctx.request.body.referralData.section4.referral_type,
+                        "Covid related": ctx.request.body.referralData.section4.is_covid,
+                        "Main eating disorder difficultie:": ctx.request.body.referralData.section4.eating_disorder_difficulties,
+                        "Food/fluid intake": ctx.request.body.referralData.section4.food_fluid_intake,
+                        "Height": ctx.request.body.referralData.section4.height,
+                        "Weight": ctx.request.body.referralData.section4.weight,
+                        "Main reason for making referral ": ctx.request.body.referralData.section4.reason_for_referral,
+                        "Info about issue:": ctx.request.body.referralData.section4.referral_issues,
+                        "Has anything helped": ctx.request.body.referralData.section4.has_anything_helped,
+                        "Triggers": ctx.request.body.referralData.section4.any_particular_trigger,
+                        "Disabilities / difficulties": ctx.request.body.referralData.section4.disabilities,
+                        //Section5
+                        "Contact person": ctx.request.body.referralData.section2.contact_person,
+                        "Contact through": ctx.request.body.referralData.section2.contact_preferences.toString(),
+                    },
+                ];
+                const csv = parse(dataCsv, csvHeader);
+                console.log(csv)
+
                 const data = {
                     from: config.email_from_address,
                     to: toAddress,
-                    subject: '[SECURE] Sefton & Liverpool CAMHS - Referral Details',
+                    subject: 'SAMPLE- [SECURE] Sefton & Liverpool CAMHS - Referral Details',
                     attachments: [{
                         filename: ctx.request.body.refCode + ".pdf",
                         content: sendReferralStatus,
                         contentType: 'application/pdf'
-                    }],
+                    }, {
+                        filename: ctx.request.body.refCode+".csv",
+                        content: Buffer.from(csv).toString('base64'),
+                    },],
                     html: htmlTemplate,
                 };
 
