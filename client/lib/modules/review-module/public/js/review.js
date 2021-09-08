d8dc9b4110071040dcc7f4d9a9f44cd8b33ace61
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-09-08 20:39:38 +0530

    fix-button enabled issue

--- client/lib/modules/review-module/public/js/review.js
+++ client/lib/modules/review-module/public/js/review.js
@@ -247,8 +247,8 @@ $(document).ready(function () {
             },
 
             save: function () {
+                debugger;
                 var buttonElem = document.querySelector('#acceptBtn');
-                buttonElem.disabled = true;
                 this.isFormSubmitted = true;
                 this.payloadData.contactPreference = this.contactPref;
                 this.payloadData.contact_person = this.contact_person;
@@ -266,6 +266,7 @@ $(document).ready(function () {
                             this.payloadData.referral_provider = "Alder Hey - Liverpool CAMHS";
                         }
 
+                        buttonElem.setAttribute('disabled', true)
                         var trimmedPayload = trimObj(this.payloadData);
                         $.ajax({
                             url: API_URI + "/saveReview",
@@ -281,23 +282,15 @@ $(document).ready(function () {
                             },
                             error: function (error) {
                                 $('#loader').removeClass('d-block').addClass('d-none');
-                                buttonElem.disabled = false;
+                                buttonElem.removeAttribute('disabled')
                                 if (error) {
                                     showError(error.responseJSON.message, error.status);
                                 }
                             }
                         });
 
-                        //var successData = apiCallPost('post', '/saveReview', this.payloadData);
-                        // if (Object.keys(successData)) {
-                        //     location.href = "/acknowledge";
-                        //     this.isFormSubmitted = false;
-                        //     $('#loader').hide();
-                        // } else {
-                        //     $('#loader').hide();
-                        // }
                     } else {
-                        // scrollToInvalidInput();
+                        buttonElem.removeAttribute('disabled')
                         return false;
                     }
 
@@ -318,7 +311,7 @@ $(document).ready(function () {
                         else if (this.section1Data.selected_service == "") {
                             this.payloadData.referral_provider = "Alder Hey - Liverpool CAMHS";
                         }
-
+                        buttonElem.setAttribute('disabled', true);
                         var trimmedPayload = trimObj(this.payloadData);
                         $.ajax({
                             url: API_URI + "/saveReview",
@@ -334,22 +327,15 @@ $(document).ready(function () {
                             },
                             error: function (error) {
                                 $('#loader').removeClass('d-block').addClass('d-none');
-                                buttonElem.disabled = true;
+                                buttonElem.removeAttribute('disabled')
                                 if (error) {
                                     showError(error.responseJSON.message, error.status);
                                 }
                             }
                         });
-                        // var successData = apiCallPost('post', '/saveReview', this.payloadData);
-                        // if (Object.keys(successData)) {
-                        //     location.href = "/acknowledge";
-                        //     this.isFormSubmitted = false;
-                        // } else {
-                        //     $('#loader').hide();
-                        // }
                     } else {
                         // scrollToInvalidInput();
-                        buttonElem.disabled = true;
+                        buttonElem.removeAttribute('disabled')
                         return false;
                     }
                 }
