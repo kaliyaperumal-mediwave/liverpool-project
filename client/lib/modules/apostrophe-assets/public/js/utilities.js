bc4c50c470778b51191c4267f7f31350aa9d12da
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2021-10-26 14:33:28 +0530

    fix-#666066493

--- client/lib/modules/apostrophe-assets/public/js/utilities.js
+++ client/lib/modules/apostrophe-assets/public/js/utilities.js
@@ -724,6 +724,20 @@ function openApps() {
     location.href = window.location.origin + '/apps';
 }
 
+// Where el is the DOM element you'd like to test for visibility
+function isHidden(el) {
+    var style = window.getComputedStyle(el);
+    return (style.display === 'none')
+}
+
+window.onbeforeunload = function (event) {
+    var resumeBtn = document.getElementById('resumeButton');
+    var isResumeHidden = isHidden(resumeBtn);
+    if (!isResumeHidden) {
+        resumeBtn.style.display = "none";
+    }
+};
+
 // function downloadJson() {
 //     var API_URI = "/modules/admin-module";
 //     console.log("4343");
