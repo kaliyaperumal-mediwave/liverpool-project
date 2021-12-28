e7923bf7242dc7960d8b89ce3123bf79f455ccb9
Thiru Prasath <thiru@mindwaveventures.com>
2021-12-28 12:08:07 +0530

    [fix] - sec 1 issue

--- server/app/controllers/referralControler.js
+++ server/app/controllers/referralControler.js
@@ -116,7 +116,7 @@ exports.eligibility = ctx => {
             {
               model: ctx.orm().Referral,
               nested: true,
-              as: 'family',
+              as: 'parent',
             },
           ],
           where: {
@@ -124,7 +124,7 @@ exports.eligibility = ctx => {
           },
         }).then((userResult) => {
 
-          var childId = userResult[0].family[0].ChildParents.parentId;
+          var childId = userResult[0].parent[0].ChildParents.parentId;
           return user.update({
             child_dob: ctx.request.body.child_Dob,
             registered_gp: ctx.request.body.registered_gp,
