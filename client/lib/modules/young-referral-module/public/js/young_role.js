7f7355ddf6c720af3a73f361d17cc4fd6120b42e
Rajkumar Manogar <rajkumar@mindwaveventures.com>
2022-06-16 15:37:04 +0530

    [fix]-section 1 , 2 email field mandatory

--- client/lib/modules/young-referral-module/public/js/young_role.js
+++ client/lib/modules/young-referral-module/public/js/young_role.js
@@ -1155,7 +1155,7 @@ $(document).ready(function () {
                 console.log(this.elgibilityObj)
                 if (role === 'professional') {
                     this.elgibilityObj.profregistered_gp = this.elgibilityObj.regProfGpTxt;
-                    if (this.elgibilityObj.profFirstName && this.elgibilityObj.proflastName && this.elgibilityObj.profContactNumber && this.dynamicRegexPattern.test(this.elgibilityObj.profContactNumber) && this.elgibilityObj.profProfession) {
+                    if (this.elgibilityObj.profFirstName && this.elgibilityObj.proflastName && this.elgibilityObj.profEmail && this.elgibilityObj.profContactNumber && this.dynamicRegexPattern.test(this.elgibilityObj.profContactNumber) && this.elgibilityObj.profProfession) {
                         if (this.elgibilityObj.profAddress || this.professionalManualAddress.length) {
                             this.elgibilityObj.professionalManualAddress = this.professionalManualAddress;
                             if (this.elgibilityObj.profEmail) {
